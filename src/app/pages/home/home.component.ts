92f4c1fbf2aa177fad830dcc75358fed3f75bd36
Stevens Garcia <smalmsteeng@gmail.com>
2020-06-26 23:04:13 -0500

    Removes unnecesary top5movies modules
    - Better integration as a child route inside home module

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -8,10 +7,9 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.router.navigate(['/movies']);
   }
 
 }
